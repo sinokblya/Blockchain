1dcc0f1d86102e7e6dca1b0180d9d4e3f32b8a48
Labintsev A I <andrej.labintsev@yandex.ru>
2023-12-05 19:20:31 +0300

    Create Tree.js

---
+++ Tree.js
@@ -0,0 +1,30 @@
+/**
+ * Seminar 2.3 Binary search tree
+ */
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    addNode(node){
+        // TODO 1 Implement 
+    }
+
+    hasNode(data){
+        // TODO 2 Implement 
+    }
+}
+
+
+
+module.exports = { Node, Tree }
